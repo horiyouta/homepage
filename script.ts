234e23455647a93a3dc00be50e9fa1b56f8d75a1
horiyouta <109045913+horiyouta@users.noreply.github.com>
2023-12-11 17:13:03 +0900

    12/11 17:12

--- script.ts
+++ script.ts
@@ -3,9 +3,11 @@ const getData = (number: number) => {
     fetch(`https://api.scratch.mit.edu/users/horiyouta/projects?limit=40&offset=${number * 40}`)
         .then(res => res.json())
         .then(resData => {
-            data.push(...resData);
-            if (number == 26) Deno.writeTextFile(`./data.js`, `const data = ${JSON.stringify(data)}`);
-            else getData(number + 1);
+            if (resData == undefined || resData.length)Deno.writeTextFile(`./data.js`, `const data = ${JSON.stringify(data)}`);
+            else {
+                data.push(...resData);
+                getData(number + 1);
+            }
         });
 }
 getData(0);
